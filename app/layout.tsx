cc369639f82a4e9e55101d96d559faa20f8eba8e
João Gabriel da Conceição Santos <gabrieudev@gmail.com>
2025-02-04 12:25:26 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+import { SiteHeader } from "@/components/site-header";
+import { SiteFooter } from "@/components/site-footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Agenda Digital",
+  description: "Agenda Digital",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR" className="h-full" suppressHydrationWarning>
+      <body className={`${inter.className} flex min-h-screen flex-col`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <SiteHeader />
+          <main className="flex-1">{children}</main>
+          <SiteFooter />
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
