926908bcb31319c2d41b39406863c88560b515c5
João Gabriel da Conceição Santos <gabrieudev@gmail.com>
2025-03-12 11:44:27 -0300

    fix: use selfi-closing tag

--- components/commitments/notifications-dialog.tsx
+++ components/commitments/notifications-dialog.tsx
@@ -28,44 +28,60 @@ export default function NotificationsDialog({
   commitment,
   refreshNotifications,
 }: NotificationsDialogProps) {
-  const [view, setView] = useState<"list" | "edit" | "create" | "invite">("list");
+  const [view, setView] = useState<"list" | "edit" | "create" | "invite">(
+    "list"
+  );
   const [selectedNotification, setSelectedNotification] =
     useState<Notification | null>(null);
   const [message, setMessage] = useState("");
   const [dueDate, setDueDate] = useState<Date | null>(null);
   const [users, setUsers] = useState<User[] | null>(null);
-  const [usersPage, setUsersPage] = useState<number>(0)
+  const [usersPage, setUsersPage] = useState<number>(0);
   const [usersPageSize, setUsersPageSize] = useState(10);
-  const [searchParam, setSearchParam] = useState("")
-  const [activeNotification, setActiveNotification] = useState<Notification | undefined>(undefined)
-  const [activeNotificationInvitations, setActiveNotificationInvitations] = useState<NotificationInvitation[] | undefined>(undefined)
-  const [invitationsPage, setInvitationsPage] = useState<number>(0)
+  const [searchParam, setSearchParam] = useState("");
+  const [activeNotification, setActiveNotification] = useState<
+    Notification | undefined
+  >(undefined);
+  const [activeNotificationInvitations, setActiveNotificationInvitations] =
+    useState<NotificationInvitation[] | undefined>(undefined);
+  const [invitationsPage, setInvitationsPage] = useState<number>(0);
   const [invitatationsPageSize, setInvitatationsPageSize] = useState(10);
 
-  useEffect(()=> {
-    api.getUsers(usersPage, usersPageSize, searchParam)
-      .then(
-        (response) => setUsers(response.content)
-      )
-  }, [usersPage, usersPageSize, searchParam, setUsers])
+  useEffect(() => {
+    api
+      .getUsers(usersPage, usersPageSize, searchParam)
+      .then((response) => setUsers(response.content));
+  }, [usersPage, usersPageSize, searchParam, setUsers]);
 
   useEffect(() => {
     if (activeNotification) {
       console.log("teste");
-      
-      api.getAllNotificationInvitations(invitationsPage, invitatationsPageSize, undefined, undefined, activeNotification.id)
-        .then(
-          (response) => setActiveNotificationInvitations(response.content)
+
+      api
+        .getAllNotificationInvitations(
+          invitationsPage,
+          invitatationsPageSize,
+          undefined,
+          undefined,
+          activeNotification.id
         )
+        .then((response) => setActiveNotificationInvitations(response.content));
     }
-  }, [activeNotification, invitationsPage, invitatationsPageSize, setActiveNotificationInvitations])
+  }, [
+    activeNotification,
+    invitationsPage,
+    invitatationsPageSize,
+    setActiveNotificationInvitations,
+  ]);
 
   const handleCreate = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!commitment || !dueDate) return;
 
-    const adjustedDueDate = new Date(dueDate.getTime() - dueDate.getTimezoneOffset() * 60000).toISOString()
-    
+    const adjustedDueDate = new Date(
+      dueDate.getTime() - dueDate.getTimezoneOffset() * 60000
+    ).toISOString();
+
     await api
       .createNotification({ message, dueDate: adjustedDueDate, commitment })
       .then(() =>
@@ -90,11 +106,17 @@ export default function NotificationsDialog({
   const handleEdit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!selectedNotification || !dueDate) return;
-    
-    const adjustedDueDate = new Date(dueDate.getTime() - dueDate.getTimezoneOffset() * 60000).toISOString()
-   
+
+    const adjustedDueDate = new Date(
+      dueDate.getTime() - dueDate.getTimezoneOffset() * 60000
+    ).toISOString();
+
     await api
-      .updateNotification({ ...selectedNotification, message, dueDate: adjustedDueDate })
+      .updateNotification({
+        ...selectedNotification,
+        message,
+        dueDate: adjustedDueDate,
+      })
       .then(() =>
         toast({
           title: "Sucesso",
@@ -137,13 +159,19 @@ export default function NotificationsDialog({
   };
 
   const handleSearchChange = (param: string) => {
-    setSearchParam(param)
-  }
+    setSearchParam(param);
+  };
 
   const handleRefreshInvitations = () => {
     if (activeNotification) {
       api
-        .getAllNotificationInvitations(invitationsPage, invitatationsPageSize, undefined, undefined, activeNotification.id)
+        .getAllNotificationInvitations(
+          invitationsPage,
+          invitatationsPageSize,
+          undefined,
+          undefined,
+          activeNotification.id
+        )
         .then((response) => setActiveNotificationInvitations(response.content))
         .catch(() =>
           toast({
@@ -156,84 +184,98 @@ export default function NotificationsDialog({
   };
 
   return (
-    <Dialog open={open} onOpenChange={(open) => { 
-      if (!open) setView("list"); 
-      onOpenChange(open);
-    }}>
+    <Dialog
+      open={open}
+      onOpenChange={(open) => {
+        if (!open) setView("list");
+        onOpenChange(open);
+      }}
+    >
       <DialogContent>
         <DialogHeader>
           <DialogTitle>
             {view === "list"
               ? "Notificações"
-              : view === "invite" 
-              ? "Convites" 
-              : view === "edit" ?
-              "Editar Notificação"
+              : view === "invite"
+              ? "Convites"
+              : view === "edit"
+              ? "Editar Notificação"
               : "Criar Notificação"}
           </DialogTitle>
         </DialogHeader>
         {view === "list" ? (
           <div className="space-y-2 max-h-64 overflow-y-auto">
             {notifications.length > 0 ? (
-            notifications.map((notif) => (
-              <div key={notif.id} className="flex justify-between p-2 border rounded-lg items-center">
-                  <span className="flex-1 break-words mr-2 overflow-hidden text-ellipsis">{notif.message} - {new Date(notif.dueDate).toLocaleString()}</span>
+              notifications.map((notif) => (
+                <div
+                  key={notif.id}
+                  className="flex justify-between p-2 border rounded-lg items-center"
+                >
+                  <span className="flex-1 break-words mr-2 overflow-hidden text-ellipsis">
+                    {notif.message} - {new Date(notif.dueDate).toLocaleString()}
+                  </span>
                   <div className="flex space-x-2">
-                  <Button
-                    
-                    size="sm"
-                    variant="ghost"
-                    onClick={() => {
-                      setView("edit");
-                      setSelectedNotification(notif);
-                      setMessage(notif.message);
-                      setDueDate(new Date(notif.dueDate));
-                    }}
-                  >
-                  <Pencil className="w-5 h-5" />
-                  </Button>
-                  <Button
-                    
-                    size="sm"
-                    variant="ghost"
-                    onClick={() => handleDelete(notif.id)}
-                  >
-                  <Trash2 className="w-5 h-5" />
-                  </Button>
-                  <Button
-                    
-                    size="sm"
-                    variant="ghost"
-                    onClick={() => {setActiveNotification(notif); setView("invite")}}
-                  >
-                  <Share2 className="w-5 h-5" />
-                  </Button>
+                    <Button
+                      size="sm"
+                      variant="ghost"
+                      onClick={() => {
+                        setView("edit");
+                        setSelectedNotification(notif);
+                        setMessage(notif.message);
+                        setDueDate(new Date(notif.dueDate));
+                      }}
+                    >
+                      <Pencil className="w-5 h-5" />
+                    </Button>
+                    <Button
+                      size="sm"
+                      variant="ghost"
+                      onClick={() => handleDelete(notif.id)}
+                    >
+                      <Trash2 className="w-5 h-5" />
+                    </Button>
+                    <Button
+                      size="sm"
+                      variant="ghost"
+                      onClick={() => {
+                        setActiveNotification(notif);
+                        setView("invite");
+                      }}
+                    >
+                      <Share2 className="w-5 h-5" />
+                    </Button>
+                  </div>
                 </div>
-              </div>
               ))
             ) : (
-              <p className="text-center text-gray-500">Nenhuma notificação encontrada</p>
+              <p className="text-center text-gray-500">
+                Nenhuma notificação encontrada
+              </p>
             )}
-            <Button className="mt-2 w-full" onClick={() =>{ setView("create"); setMessage(""); setDueDate(null)}}>
+            <Button
+              className="mt-2 w-full"
+              onClick={() => {
+                setView("create");
+                setMessage("");
+                setDueDate(null);
+              }}
+            >
               Criar Notificação
             </Button>
           </div>
         ) : view === "invite" ? (
-          <Invite  
-            notification={activeNotification} 
+          <Invite
+            notification={activeNotification}
             onSearchChange={handleSearchChange}
             users={users}
             invitations={activeNotificationInvitations}
             onRefreshInvitations={handleRefreshInvitations}
-          >      
-          </Invite>
-        )
-        
-        : (
+          />
+        ) : (
           <form
             onSubmit={view === "create" ? handleCreate : handleEdit}
             className="space-y-4"
-          > 
+          >
             <Label>Mensagem</Label>
             <Input
               value={message}
@@ -244,7 +286,13 @@ export default function NotificationsDialog({
             <Input
               type="datetime-local"
               value={
-                dueDate ? new Date(dueDate.getTime() - dueDate.getTimezoneOffset() * 60000 ).toISOString().slice(0, 16) : ""
+                dueDate
+                  ? new Date(
+                      dueDate.getTime() - dueDate.getTimezoneOffset() * 60000
+                    )
+                      .toISOString()
+                      .slice(0, 16)
+                  : ""
               }
               onChange={handleDueDateChange}
               placeholder="Data de vencimento"
