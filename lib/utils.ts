2d0d11a2276c858db842f67c35201253ff84fe79
Gustavo Henrique <gh8674723@gmail.com>
2025-03-12 11:08:49 -0300

    fix: adjusting header and footer width

--- lib/utils.ts
+++ lib/utils.ts
@@ -13,8 +13,11 @@ export async function getAuthenticatedUser() {
 }
 
 export async function isAuthenticatedUserAdmin() {
-  const user = await api.getMe();
+  try {const user = await api.getMe();
   const userRoles = await api.getRoles(0, 20, user.id);
 
-  return userRoles.find((role: any) => role.name === "ADMIN");
+  return userRoles.find((role: any) => role.name === "ADMIN");} catch (error) {
+    console.log(error);
+    
+  }
 }
